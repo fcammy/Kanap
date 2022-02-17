da221fb86ed8044b4d0c779f9b561b44ce4428fe
fcammy25 <facinet.camara@gmail.com>
2022-02-17 16:44:10 +0000

    added toast feature and fixed bugs

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,37 +1,59 @@
 // DOM elements
-const firstName = document.querySelector('#firstName');
-const lastName = document.getElementById('lastName');
-const address = document.querySelector('#address');
-const city = document.querySelector('#city');
-const email = document.getElementById('email');
-const btnOrder = document.querySelector('#order');
-const products = document.querySelector('.cart__item');
-const deleteBtn = document.querySelector('.deleteItem');
+const firstName = document.querySelector("#firstName");
+const lastName = document.getElementById("lastName");
+const address = document.querySelector("#address");
+const city = document.querySelector("#city");
+const email = document.getElementById("email");
+const btnOrder = document.querySelector("#order");
+const products = document.querySelector(".cart__item");
+let deleteBtns,
+  qtyInputs = [];
 
+// function to calculate the total price of the products in the cart and update products number in cart
 
+const updateTotal = () => {
+  let cartProducts = JSON.parse(localStorage.getItem("cart"));
+  let cartQuantity = document.querySelector("#totalQuantity");
+  cartQuantity.innerHTML = cartProducts.length;
 
-// get items from local storage and display them on the page
+  let cartItems = JSON.parse(localStorage.getItem("cart"));
+  let total = document.querySelector("#totalPrice");
+  let totalPrice = 0;
+  if (cartItems) {
+    for (let i = 0; i < cartItems.length; i++) {
+      // convert array object into number
+      let priceString = cartItems[i].price.toString();
+      let price = priceString.substring();
+      let convertedPrice = parseInt(price);
+      let itemPrice = convertedPrice * cartItems[i].qty;
+      totalPrice += itemPrice;
+      total.innerHTML = totalPrice;
+    }
+  }
+};
+
+// function to display front end data to page and update page when product added or removed
 
 const displayCart = () => {
-    
-    const cartItems = localStorage.getItem('cart'); 
+  const cartItems = localStorage.getItem("cart");
 
-    let cart = cartItems ? JSON.parse(cartItems) : [];
-    cart = [...cart];
+  let cart = cartItems ? JSON.parse(cartItems) : [];
+  cart = [...cart];
 
-    cart.forEach(item => {
+  products.innerHTML = "";
 
-        //console.log(item);
+  cart.forEach((item) => {
+    //console.log(item);
 
-        // displaying products on the DOM
-        
-        products.innerHTML += `
+    // displaying products on the DOM
+
+    products.innerHTML += `
 
         <section class="cart">
             <section id="cart__items>
                 <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
                 <div class="cart__item__img">
-                <img src="${item.imageUrl}" alt="Photo of a sofa">
+                    <img src="${item.imageUrl}" alt="Photo of a sofa">
                 </div>
                 <div class="cart__item__content">
                 <div class="cart__item__content__description">
@@ -39,7 +61,7 @@ const displayCart = () => {
                     <p class="product__color">${item.color}</p>
                     <p class="product__price">€ ${item.price}</p>
                 </div>
-                <div class="cart__item__content__settings">
+                <div data-cartId="${item.cartId}" class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                     <p>Qté : </p>
                     <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.qty}">
@@ -52,85 +74,60 @@ const displayCart = () => {
             </article>
         </section>
       </section>
-        `
-    
-        
-    });
-             
-      }
-    
+        `;
+  });
+
+  updateTotal();
+
+  deleteBtns = document.querySelectorAll(".deleteItem");
+  qtyInputs = document.querySelectorAll(".itemQuantity");
+
+  deleteBtns.forEach((btn) => btn.addEventListener("click", removeProduct));
+  qtyInputs.forEach((input) => {
+    input.addEventListener("keyup", updateQuantity);
+    input.addEventListener("change", updateQuantity);
+  });
+
+  // console.log(qtyInputs)
+};
 displayCart();
-
-// cart badge shows number of items in the cart
-
-const cartNum = () => {
-
-    const localStorageProducts = localStorage.getItem('cart');
-    let cartProducts = JSON.parse(localStorageProducts);
-    let cartQuantity = document.querySelector('#totalQuantity');
-    cartQuantity.innerHTML += cartProducts.length;
-
-}
-
-cartNum();
-
-
-
 
 // function to remove products from cart and update the localstorage
 
+function removeProduct({ target }) {
+  const cartId = Number(target.parentNode.parentNode.dataset.cartid);
 
-//deleteBtn.addEventListener('click', () => removeProduct(index));
+  let cartItems = JSON.parse(localStorage.getItem("cart"));
 
-function removeProduct() {
+  console.log({ before: cartItems });
+  cartItems = cartItems.filter((item) => item.cartId !== cartId);
+  console.log({ after: cartItems });
 
-    let cartItems = JSON.parse(localStorage.getItem('cart'));
-    cartItems.splice(index, 1);
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-    // update cart
+  localStorage.setItem("cart", JSON.stringify(cartItems));
 
-    displayCart();
+  // update cart
+
+  displayCart();
 }
- 
-
-// function to calculate the total price of the products in the cart
-
-
-function calculateTotalPrice() {
-
-    let cartItems = JSON.parse(localStorage.getItem('cart'));
-    let total = document.querySelector('#totalPrice');
-    let totalPrice = 0;
-    if (cartItems) {
-
-        for (let i = 0; i < cartItems.length; i++) {
-        
-            // convert array object into number
-            let priceString = cartItems[i].price.toString();
-            let price = priceString.substring();
-            let convertedPrice = parseInt(price);
-            let itemPrice = convertedPrice * cartItems[i].qty;
-            totalPrice += itemPrice;
-            total.innerHTML = totalPrice;
-            //console.log(totalPrice);
-
-                }
-
-    }
-}
-
-calculateTotalPrice();
 
 // change product quantity then update the price
 
-function updateQuantity(i, value) {
-    let cartItems = JSON.parse(localStorage.getItem('cart'));
-    cartItems[i].qty = e.target.parseInt(value);
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-    // update cart
-    displayCart();
-    // update the total price
-    calculateTotalPrice();
-}
+function updateQuantity({ target }) {
+  const cartId = Number(target.parentNode.parentNode.dataset.cartid);
 
-updateQuantity();
+  let cartItems = JSON.parse(localStorage.getItem("cart"));
+
+  console.log({ before: cartItems });
+  cartItems = cartItems.map((item) => {
+    if (item.cartId === cartId) {
+      item.qty = Number(target.value);
+      return item;
+    }
+
+    return item;
+  });
+  console.log({ after: cartItems });
+
+  localStorage.setItem("cart", JSON.stringify(cartItems));
+  displayCart();
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,79 +1,100 @@
 // Access DOM
-const title = document.getElementsByTagName('title');
-const sofaImg = document.querySelector('.item__img');
-const sofaName = document.querySelector('#title');
-const sofaPrice = document.getElementById('price');
-const sofaDescription = document.getElementById('description');
-const sofaColors = document.getElementById('colors');
-const quantity = document.querySelector('#quantity');
-const addToCartBtn = document.querySelector('#addToCart');
-
-
-
+const title = document.getElementsByTagName("title");
+const sofaImg = document.querySelector(".item__img");
+const sofaName = document.querySelector("#title");
+const sofaPrice = document.getElementById("price");
+const sofaDescription = document.getElementById("description");
+const sofaColors = document.getElementById("colors");
+const quantity = document.querySelector("#quantity");
+const addToCartBtn = document.querySelector("#addToCart");
 
 // Getting a single product
 
-const getProduct =  async () => {
+const getProduct = async () => {
+  const productId = document.location.search.split("=")[1];
 
-    const productId = document.location.search.split("=")[1];
+  const response = await fetch(
+    "http://localhost:3000/api/products/" + productId
+  );
+  const data = await response.json();
 
-    
-    const response = await fetch('http://localhost:3000/api/products/' + productId);
-    const data = await response.json();
+  // // Display sofa details on the DOM
 
-    //console.log(data); // shows data on the console
-    
-    // // Display sofa details on the DOM
+  document.title = `${data.name}`;
+  title.innerHTML = `${data.name}`;
+  sofaImg.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+  sofaName.innerHTML = `${data.name}`;
+  sofaPrice.innerHTML = `${data.price}`;
+  sofaDescription.innerHTML = `${data.description}`;
 
-        document.title = `${data.name}`;
-        title.innerHTML = `${data.name}`;
-        sofaImg.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-        sofaName.innerHTML = `${data.name}`;
-        sofaPrice.innerHTML = `${data.price}`;
-        sofaDescription.innerHTML = `${data.description}`;
+  data.colors.forEach((c) => {
+    sofaColors.innerHTML += `<option value="${c}">${c}</option>`;
+  });
 
-        data.colors.forEach(c => {
-            sofaColors.innerHTML += `<option value="${c}">${c}</option>`
-        })
-
-    return data;
-}
+  return data;
+};
 
 let selectedColor;
 let selectedQty = 0;
 
-sofaColors.addEventListener('change', ({target}) => selectedColor = target.value);
-quantity.addEventListener('change', ({target}) => selectedQty = +target.value);
+sofaColors.addEventListener(
+  "change",
+  ({ target }) => (selectedColor = target.value)
+);
+quantity.addEventListener(
+  "change",
+  ({ target }) => (selectedQty = +target.value)
+);
 
+const addToCart = ({ name, price, imageUrl, _id }) => {
+  const product = {
+    name,
+    price,
+    imageUrl,
+    _id,
+    cartId: new Date().getTime(), // get random sofa id for further processing
+    color: selectedColor,
+    qty: selectedQty,
+  };
 
+  // update the quantity when products of same colour and id added to the cart
 
-const addToCart = ({name, price, imageUrl, _id }) => {
-    
-    const product = {
-        name,
-        price, 
-        imageUrl, 
-        _id,
-        cartId: new Date().getTime(), // get random sofa id for further processing
-        color: selectedColor,
-        qty: selectedQty
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const cartItem = cart.find((item) => {
+    if (item._id === product._id && item.color === product.color) {
+      item.qty += Number(product.qty);
+
+      return item;
     }
+  });
 
-    const cartItems = localStorage.getItem('cart'); 
+  //console.log({cartItem})
 
-    let cart = cartItems ? JSON.parse(cartItems) : [];
-
+  if (cartItem) {
+    cart = cart.filter((item) => item.cartId !== cartItem.cartId);
+    cart = [...cart, cartItem];
+  } else {
     cart = [...cart, product];
-    localStorage.setItem('cart',JSON.stringify(cart))
+  }
 
-    console.log(cart)
+  localStorage.setItem("cart", JSON.stringify(cart));
 
-}
+  // console.log(product)
+  // console.log(cart)
 
-(async() => {
-    const product = await getProduct();
+  // add toast notification
 
-    addToCartBtn.addEventListener('click', () =>  addToCart(product))
+  let notification = document.getElementById("confirmation");
+  notification.innerHTML = "Added to cart";
+  notification.className = "toast";
+  setTimeout(function () {
+    notification.className = notification.className.replace("toast", " ");
+  }, 2000);
+};
 
-})()
+(async () => {
+  const product = await getProduct();
 
+  addToCartBtn.addEventListener("click", () => addToCart(product));
+})();
