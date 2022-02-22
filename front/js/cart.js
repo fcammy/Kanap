c6c0c9efa001ce01c06410d37d12ba94c5f520ad
fcammy25 <facinet.camara@gmail.com>
2022-02-22 22:33:00 +0000

    post request added

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,9 +4,10 @@ const lastName = document.getElementById("lastName");
 const address = document.querySelector("#address");
 const city = document.querySelector("#city");
 const email = document.getElementById("email");
-const btnOrder = document.querySelector("#order");
+const btnOrder = document.getElementById("order");
 const products = document.querySelector(".cart__item");
-const form = document.querySelector("cart__order__form");
+const form = document.querySelector(".cart__order__form");
+const orderId = document.getElementById("orderId");
 let deleteBtns,
   qtyInputs = [];
 
@@ -124,84 +125,173 @@ function updateQuantity({ target }) {
 
     return item;
   });
-  
 
   localStorage.setItem("cart", JSON.stringify(cartItems));
   displayCart();
 }
 
+// VALIDATION INITIALASATION
+
+let isFirstNameValid = false;
+let isLastNameValid = false;
+let isEmailValid = false;
+let isAddressValid = false;
+let isCityValid = false;
 
 // VALIDATE FIRST NAME
 
-firstName.addEventListener('blur', () => {
+firstName.addEventListener("blur", () => {
+  const firstNamePattern = /^[a-zA-Z]+$/;
+  const firstNameError = document.getElementById("firstNameErrorMsg");
 
- const firstNamePattern = /^[a-zA-Z]+$/;
- const firstNameError = document.getElementById('firstNameErrorMsg');
-
- if(firstNamePattern.test(firstName.value)) {
-   firstNameError.textContent = 'Name entered is valid';
-
- } else {
-   firstNameError.textContent = 'Name must not contain a number';
- }
-
+  if (firstNamePattern.test(firstName.value)) {
+    firstNameError.textContent = "Name entered is valid";
+    isFirstNameValid = true;
+  } else {
+    firstNameError.textContent = "Name must not contain a number";
+    return false;
+  }
 });
 
 // VALIDATE LAST NAME
 
-lastName.addEventListener('blur', () => {
-
+lastName.addEventListener("blur", () => {
   const lastNamePattern = /^[a-zA-Z]+$/;
-  const lastNameError = document.getElementById('lastNameErrorMsg');
- 
-  if(lastNamePattern.test(lastName.value)) {
-    lastNameError.textContent = 'Last Name entered is valid';
- 
+  const lastNameError = document.getElementById("lastNameErrorMsg");
+
+  if (lastNamePattern.test(lastName.value)) {
+    lastNameError.textContent = "Last Name entered is valid";
+    isLastNameValid = true;
   } else {
-    lastNameError.textContent = 'Last Name must not contain a number';
+    lastNameError.textContent = "Last Name must not contain a number";
+    return false;
   }
- 
- });
- 
+});
 
- // VALIDATE ADDRESS 
+// VALIDATE ADDRESS
 
- address.addEventListener('blur', () => {
+address.addEventListener("blur", () => {
+  const addressPattern = /^\s*\S+(?:\s+\S+){2}/;
+  const addressError = document.getElementById("addressErrorMsg");
 
-   const addressPattern = /^\s*\S+(?:\s+\S+){2}/;
-   const addressError = document.getElementById('addressErrorMsg');
-
-   if(addressPattern.test(address.value)) {
-     addressError.textContent = 'Address entered is valid';
-   } else {
-     addressError.textContent = 'You must enter a valid address';
-   }
- });
-
- // VALIDATE CITY
-
- city.addEventListener('blur', () => {
-
-  const cityPattern =  /^[a-zA-Z]+$/;
-  const cityError = document.getElementById('cityErrorMsg');
-
-  if(cityPattern.test(city.value)) {
-    cityError.textContent = 'City entered is valid';
+  if (addressPattern.test(address.value)) {
+    addressError.textContent = "Address entered is valid";
+    isAddressValid = true;
   } else {
-    cityError.textContent = 'You must enter a valid city';
+    addressError.textContent = "You must enter a valid address";
+    return false;
+  }
+});
+
+// VALIDATE CITY
+
+city.addEventListener("blur", () => {
+  const cityPattern = /^[a-zA-Z]+$/;
+  const cityError = document.getElementById("cityErrorMsg");
+
+  if (cityPattern.test(city.value)) {
+    cityError.textContent = "City entered is valid";
+    isCityValid = true;
+  } else {
+    cityError.textContent = "You must enter a valid city";
+    return false;
   }
 });
 
 // VALIDATE EMAIL ADDRESS
 
-email.addEventListener('blur', () => {
-  const emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  const emailError = document.getElementById('emailErrorMsg');
+email.addEventListener("blur", () => {
+  const emailPattern =
+    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  const emailError = document.getElementById("emailErrorMsg");
 
-  if(emailPattern.test(email.value)) {
-    emailError.textContent = ' Email is valid';
-
+  if (emailPattern.test(email.value)) {
+    emailError.textContent = " Email is valid";
+    isEmailValid = true;
   } else {
-    emailError.textContent = ' Email is invalid';
+    emailError.textContent = " Email is invalid";
+    return false;
   }
 });
+
+// POST DATA ENTERED BY THE USER
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  let products = [];
+
+  // GET SOFA ID AND PUSH IT TO ARRAY
+
+  let cartItems = JSON.parse(localStorage.getItem("cart"));
+  for (let i = 0; i < cartItems.length; i++) {
+    products.push(cartItems[i]._id);
+  }
+  //console.log(products);
+  //STORING OBJECT INFORMATION FROM USER
+
+  let contact = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    address: address.value,
+    city: city.value,
+  };
+
+  let data = {
+    contact: contact,
+    products: products,
+  };
+
+  if (
+    isFirstNameValid &&
+    isLastNameValid &&
+    isAddressValid &&
+    isCityValid &&
+    isEmailValid
+  ) {
+    sendInfo(data);
+  }
+});
+
+// SENDING INFORMATION TO API
+
+const sendInfo = (data) => {
+  fetch("http://localhost:3000/api/products/order", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+     let  orderId = data.orderId;
+      sessionStorage.setItem("orderId", orderId);
+
+      location.replace("/front/html/confirmation.html");
+      
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    
+};
+
+
+// SHOWING ORDER ID ON CONFIRMATION PAGE
+
+orderId.innerHTML = `
+    <div class="confirmation">
+    <p>Order confirmed! <br>Your order number is: <span id="orderId">${data.orderId}</span></p>
+    </div>
+    
+`
+localStorage.removeItem('cart');
+sessionStorage.removeItem('orderId');
